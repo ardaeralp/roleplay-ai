71330734d53aefa74e0fd28b8c51f26d2b6ad9f9
Arda Eralp <32901167+ardaeralp@users.noreply.github.com>
2025-11-16 10:20:13 +0300

    Update cohere.js

--- scripts/cohere.js
+++ scripts/cohere.js
@@ -2,22 +2,26 @@ const COHERE_API_KEY = "COHERE_API_KEYINIZ";
 
 async function sendToCohere(userInput) {
     try {
-        const response = await fetch("https://api.cohere.ai/generate", {
+        const response = await fetch("https://api.cohere.ai/v2/chat", {
             method: "POST",
             headers: {
+                "Accept": "application/json",
                 "Content-Type": "application/json",
                 "Authorization": `Bearer ${COHERE_API_KEY}`
             },
             body: JSON.stringify({
-                model: "xlarge",
-                prompt: userInput,
-                max_tokens: 1000,
-                temperature: 0.7
+            model: "command-a-03-2025",
+            messages: [
+                {
+                    role: "user",
+                    content: userInput,
+                },
+            ],
             })
         });
         if (!response.ok) throw new Error("Cohere API hatası");
         const data = await response.json();
-        return data.generations[0].text;
+        return data.message.content[0].text;
     } catch (err) {
         throw new Error("Cohere Hatası: " + err.message);
     }
